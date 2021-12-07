b134f3cd876dc5d896c8fca47291d741e75d5d67
Makrand <makrandshingare@gmail.com>
2021-12-07 22:34:57 +0530

    Refactor : refactor Integer Float String max method in one Generic method

--- src/main/java/com/biz/genmax/GenericMaximum.java
+++ src/main/java/com/biz/genmax/GenericMaximum.java
@@ -2,16 +2,16 @@ package com.biz.genmax;
 
 public class GenericMaximum {
     public static void main(String[] args) {
-        Integer intResult = findIntegerMaximum(10, 20, 30);
+        Integer intResult = findMaximum(10, 20, 30);
         System.out.println(intResult);
-        Float floatResult = findFloatMaximum(10.1f, 20.2f, 30.3f);
+        Float floatResult = findMaximum(10.1f, 20.2f, 30.3f);
         System.out.println(floatResult);
-        String stringResult=findStringMaximum("ABC","PQR","XYZ");
+        String stringResult=findMaximum("ABC","PQR","XYZ");
         System.out.println(stringResult);
 
     }
 
-    public static Integer findIntegerMaximum(Integer a, Integer b, Integer c) {
+    public static <T extends Comparable> T findMaximum(T a, T b, T c) {
         if (a.compareTo(b) > 0 && a.compareTo(c) > 0) {
             return a;
         } else if (b.compareTo(a) > 0 && b.compareTo(c) > 0) {
@@ -21,22 +21,4 @@ public class GenericMaximum {
 
     }
 
-    public static Float findFloatMaximum(Float a, Float b, Float c) {
-        if (a.compareTo(b) > 0 && a.compareTo(c) > 0) {
-            return a;
-        } else if (b.compareTo(a) > 0 && b.compareTo(c) > 0) {
-            return b;
-        }
-        return c;
-
-    }
-    public static String findStringMaximum(String a, String b, String c) {
-        if (a.compareTo(b) > 0 && a.compareTo(c) > 0) {
-            return a;
-        } else if (b.compareTo(a) > 0 && b.compareTo(c) > 0) {
-            return b;
-        }
-        return c;
-
-    }
 }
--- src/test/java/com/biz/genmax/GenericMaximumTest.java
+++ src/test/java/com/biz/genmax/GenericMaximumTest.java
@@ -7,63 +7,63 @@ public class GenericMaximumTest {
     @Test
     //Test Case for the Integer where max at first Position
     void given3Integer_whenMax_returnCorrectMax_case1(){
-       Integer actualResult= GenericMaximum.findIntegerMaximum(30,20,10);
+       Integer actualResult= GenericMaximum.findMaximum(30,20,10);
        Integer expectedResult=30;
         Assert.assertEquals(expectedResult,actualResult);
     }
     @Test
         //Test Case for the Integer where max at Second Position
     void given3Integer_whenMax_returnCorrectMax_case2(){
-        Integer actualResult= GenericMaximum.findIntegerMaximum(30,40,10);
+        Integer actualResult= GenericMaximum.findMaximum(30,40,10);
         Integer expectedResult=40;
         Assert.assertEquals(expectedResult,actualResult);
     }
     @Test
         //Test Case for the Integer where max at Third Position
     void given3Integer_whenMax_returnCorrectMax_case3(){
-        Integer actualResult= GenericMaximum.findIntegerMaximum(30,20,40);
+        Integer actualResult= GenericMaximum.findMaximum(30,20,40);
         Integer expectedResult=40;
         Assert.assertEquals(expectedResult,actualResult);
     }
     @Test
         //Test Case for the Float where max at first Position
     void given3Floats_whenMax_returnCorrectMax_case1(){
-        Float actualResult= GenericMaximum.findFloatMaximum(30.1f,20.2f,10.3f);
+        Float actualResult= GenericMaximum.findMaximum(30.1f,20.2f,10.3f);
         Float expectedResult=30.1f;
         Assert.assertEquals(expectedResult,actualResult);
     }
     @Test
         //Test Case for the Float where max at Second Position
     void given3Floats_whenMax_returnCorrectMax_case2(){
-        Float actualResult= GenericMaximum.findFloatMaximum(30.1f,40.2f,10.3f);
+        Float actualResult= GenericMaximum.findMaximum(30.1f,40.2f,10.3f);
         Float expectedResult=40.2f;
         Assert.assertEquals(expectedResult,actualResult);
     }
     @Test
         //Test Case for the Float where max at Third Position
     void given3Floats_whenMax_returnCorrectMax_case3(){
-        Float actualResult= GenericMaximum.findFloatMaximum(30.1f,20.2f,40.3f);
+        Float actualResult= GenericMaximum.findMaximum(30.1f,20.2f,40.3f);
         Float expectedResult=40.3f;
         Assert.assertEquals(expectedResult,actualResult);
     }
     @Test
         //Test Case for the String where max at first Position
     void given3String_whenMax_returnCorrectMax_case1(){
-        String actualResult= GenericMaximum.findStringMaximum("abc","PQR","XYZ");
+        String actualResult= GenericMaximum.findMaximum("abc","PQR","XYZ");
         String expectedResult="abc";
         Assert.assertEquals(expectedResult,actualResult);
     }
     @Test
         //Test Case for the String where max at Second Position
     void given3String_whenMax_returnCorrectMax_case2(){
-        String actualResult= GenericMaximum.findStringMaximum("abc","pqr","XYZ");
+        String actualResult= GenericMaximum.findMaximum("abc","pqr","XYZ");
         String expectedResult="pqr";
         Assert.assertEquals(expectedResult,actualResult);
     }
     @Test
         //Test Case for the String where max at Third Position
     void given3String_whenMax_returnCorrectMax_case3(){
-        String actualResult= GenericMaximum.findStringMaximum("abc","PQR","xyz");
+        String actualResult= GenericMaximum.findMaximum("abc","PQR","xyz");
         String expectedResult="xyz";
         Assert.assertEquals(expectedResult,actualResult);
     }
