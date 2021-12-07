557787edf1d05b45d1f4efc3c35a17e15236d80b
Makrand <makrandshingare@gmail.com>
2021-12-07 21:52:09 +0530

    Added : Method for calulate Maximum Float numbers and test using 3 test cases

--- src/main/java/com/biz/genmax/GenericMaximum.java
+++ src/main/java/com/biz/genmax/GenericMaximum.java
@@ -2,19 +2,31 @@ package com.biz.genmax;
 
 public class GenericMaximum {
     public static void main(String[] args) {
-       Integer max= findIntegerMaximum(10,20,30);
-        System.out.println(max);
+        Integer intResult = findIntegerMaximum(10, 20, 30);
+        System.out.println(intResult);
+        Float floatResult = findFloatMaximum(10.1f, 20.2f, 30.3f);
+        System.out.println(floatResult);
 
     }
 
     public static Integer findIntegerMaximum(Integer a, Integer b, Integer c) {
-        if(a.compareTo(b) >0 && a.compareTo(c)>0){
-            return  a;
-        }
-        else if(b.compareTo(a)>0 && b.compareTo(c)>0){
+        if (a.compareTo(b) > 0 && a.compareTo(c) > 0) {
+            return a;
+        } else if (b.compareTo(a) > 0 && b.compareTo(c) > 0) {
             return b;
         }
         return c;
 
     }
+
+    public static Float findFloatMaximum(Float a, Float b, Float c) {
+        if (a.compareTo(b) > 0 && a.compareTo(c) > 0) {
+            return a;
+        } else if (b.compareTo(a) > 0 && b.compareTo(c) > 0) {
+            return b;
+        }
+        return c;
+
+    }
+    
 }
--- src/test/java/com/biz/genmax/GenericMaximumTest.java
+++ src/test/java/com/biz/genmax/GenericMaximumTest.java
@@ -6,23 +6,44 @@ import org.testng.annotations.Test;
 public class GenericMaximumTest {
     @Test
     //Test Case for the Integer where max at first Position
-    void given3Integrates_whenMax_returnCorrectMax_case1(){
+    void given3Integer_whenMax_returnCorrectMax_case1(){
        Integer actualResult= GenericMaximum.findIntegerMaximum(30,20,10);
        Integer expectedResult=30;
         Assert.assertEquals(expectedResult,actualResult);
     }
     @Test
         //Test Case for the Integer where max at Second Position
-    void given3Integrates_whenMax_returnCorrectMax_case2(){
+    void given3Integer_whenMax_returnCorrectMax_case2(){
         Integer actualResult= GenericMaximum.findIntegerMaximum(30,40,10);
         Integer expectedResult=40;
         Assert.assertEquals(expectedResult,actualResult);
     }
     @Test
         //Test Case for the Integer where max at Third Position
-    void given3Integrates_whenMax_returnCorrectMax_case3(){
+    void given3Integer_whenMax_returnCorrectMax_case3(){
         Integer actualResult= GenericMaximum.findIntegerMaximum(30,20,40);
         Integer expectedResult=40;
+        Assert.assertEquals(expectedResult,actualResult);
+    }
+    @Test
+        //Test Case for the Float where max at first Position
+    void given3Floats_whenMax_returnCorrectMax_case1(){
+        Float actualResult= GenericMaximum.findFloatMaximum(30.1f,20.2f,10.3f);
+        Float expectedResult=30.1f;
+        Assert.assertEquals(expectedResult,actualResult);
+    }
+    @Test
+        //Test Case for the Float where max at Second Position
+    void given3Floats_whenMax_returnCorrectMax_case2(){
+        Float actualResult= GenericMaximum.findFloatMaximum(30.1f,40.2f,10.3f);
+        Float expectedResult=40.2f;
+        Assert.assertEquals(expectedResult,actualResult);
+    }
+    @Test
+        //Test Case for the Float where max at Third Position
+    void given3Floats_whenMax_returnCorrectMax_case3(){
+        Float actualResult= GenericMaximum.findFloatMaximum(30.1f,20.2f,40.3f);
+        Float expectedResult=40.3f;
         Assert.assertEquals(expectedResult,actualResult);
     }
 }
